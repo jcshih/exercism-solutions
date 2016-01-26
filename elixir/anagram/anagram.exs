97cfa06d6d21db266edc9c8b75970b21108463d6
Jonathan Shih <joncshih@gmail.com>
2016-01-26 01:22:01 -0800

    elixir anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,19 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    base_sig = signature(base)
+
+    candidates
+    |> Enum.reject(fn(c) -> String.downcase(c) == base end)
+    |> Enum.filter(fn(c) -> signature(c) == base_sig end)
+  end
+
+  @spec signature(String.t) :: String.t
+  def signature(s) do
+    s
+    |> String.downcase
+    |> String.to_char_list
+    |> Enum.sort
+    |> List.to_string
   end
 end
