be70783e43c75cb836587d9ecd5aba2eb47fe756
Jonathan Shih <joncshih@gmail.com>
2016-01-26 13:50:51 -0800

    erlang leap iteration

--- erlang/leap/leap.erl
+++ erlang/leap/leap.erl
@@ -2,11 +2,7 @@
 -export([leap_year/1]).
 
 -spec leap_year(integer()) -> boolean().
-leap_year(Y) when is_integer(Y) ->
-    case Y rem 4 of
-        0 -> case Y rem 100 of
-                 0 -> Y rem 400 =:= 0;
-                 _ -> true
-             end;
-        _ -> false
-    end.
+leap_year(Y) when is_integer(Y), Y rem 400 =:= 0 -> true;
+leap_year(Y) when is_integer(Y), Y rem 100 =:= 0 -> false;
+leap_year(Y) when is_integer(Y), Y rem 4 =:= 0 -> true;
+leap_year(_) -> false.
