c6f262e943808e6021665d80899fd8189fab5568
Jonathan Shih <joncshih@gmail.com>
2016-02-13 16:01:06 -0800

    es6 hamming

---
+++ ecmascript/hamming/hamming.js
@@ -0,0 +1,17 @@
+class Hamming {
+  zip(a, b) {
+    return a.map((x, i) => [x, b[i]]);
+  }
+
+  compute(a, b) {
+    if (a.length != b.length)
+      throw new Error('DNA strands must be of equal length.');
+
+    return this.zip(a.split(''), b.split(''))
+      .map(([x, y]) => x != y)
+      .filter((x) => x)
+      .length;
+  }
+}
+
+export default Hamming;
