9a8602208198187cd0afd2e48f152ea92609c196
Jonathan Shih <joncshih@gmail.com>
2016-02-26 14:20:48 -0800

    es6 strain iteration

--- ecmascript/strain/strain.js
+++ ecmascript/strain/strain.js
@@ -1,6 +1,16 @@
+const filter = (coll, pred, bool) => {
+  const result = [];
+  for (let i=0; i<coll.length; i++) {
+    if (pred(coll[i]) == bool) {
+      result.push(coll[i]);
+    }
+  }
+  return result;
+};
+
 const strain = {
-  keep: (coll, func) => coll.filter(func),
-  discard: (coll, func) => coll.filter(x => !func(x))
+  keep: (coll, func) => filter(coll, func, true),
+  discard: (coll, func) => filter(coll, func, false)
 };
 
 export default strain;
