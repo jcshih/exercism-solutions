67c892e6e054002814838eda2510cd921cfd25f0
Jonathan Shih <joncshih@gmail.com>
2016-03-09 21:52:20 -0800

    c# bob

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Bob
+{
+    public static string Hey (string message)
+    {
+        if (IsExclamation(message))
+            return "Whoa, chill out!";
+        if (IsQuestion(message))
+            return "Sure.";
+        if (IsSilence(message))
+            return "Fine. Be that way!";
+        return "Whatever.";
+    }
+
+    private static bool IsExclamation (string message)
+    {
+        return message == message.ToUpper() && message != message.ToLower();
+    }
+
+    private static bool IsQuestion (string message)
+    {
+        return message.Trim().EndsWith("?");
+    }
+
+    private static bool IsSilence (string message)
+    {
+        return message.Trim() == "";
+    }
+}
